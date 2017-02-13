fa7e0b3dea1efad0926fbf06afd0fa6263186a04
Robot Computer <cphsrobots@gmail.com>
2017-02-13 12:28:05 -0800

    Made significant changes to Robot.java

--- CPHS Robotics/src/org/usfirst/frc/team6662/robot/Robot.java
+++ CPHS Robotics/src/org/usfirst/frc/team6662/robot/Robot.java
@@ -6,14 +6,9 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.ADXL362;
-import edu.wpi.first.wpilibj.AnalogGyro;
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
@@ -24,89 +19,65 @@ public class Robot extends IterativeRobot {
 	Compressor compressor = new Compressor(0);
 	DoubleSolenoid solenoid = new DoubleSolenoid(0, 1);
 	Victor liftMotor = new Victor(4);
-	Accelerometer accel = /*new ADXL362(SPI.Port.kOnboardCS0, Accelerometer.Range.k4G);*/ new BuiltInAccelerometer(Accelerometer.Range.k4G);
 	Gyro gyro;
-	
-	int loop;
-	double lastGyroAngle;
-	double lastTime;
-	double currTime;
-	double averageGyroDrift;
-	
-	@Override
-	public void disabledInit() {
-		gyro.reset();
-		lastGyroAngle = gyro.getAngle();
-		
-		averageGyroDrift = 0;
-		loop = 0;
-	}
-	
-	
-	@Override
-	public void disabledPeriodic() {
-		double currGyroAngle = gyro.getAngle();
-		//update time
-		if (loop < 20) {
-			averageGyroDrift += ((currGyroAngle - lastGyroAngle) / (currTime - lastTime)) / 20.0;
-		}
-			
-		
-	}
 	
 	@Override
 	public void robotInit() {
 		gyro = new ADXRS450_Gyro();
-		compressor.setClosedLoopControl(true);
+	  	compressor.setClosedLoopControl(true);
 	//	CameraServer.getInstance().startAutomaticCapture();
-		loop = 0;
 	}
 	
 	@Override
 	public void autonomousInit() {
-		loop = 0;
 	}
 
 	@Override
 	public void autonomousPeriodic() {
-		
-		loop++;
 	}
 	
 	@Override
 	public void teleopInit() {
 		gyro.calibrate();
-		loop = 0;
 	}
 
 	@Override
 	public void teleopPeriodic() {
-		boolean trigger = joystick.getRawButton(1);
+		boolean trigger        = joystick.getRawButton(1),
+				overrideButton = joystick.getRawButton(9),
+				stopButton     = joystick.getRawButton(10),
+				modeButton     = joystick.getRawButton(11);
+		
+		double rotate = joystick.getX(),
+				angle = gyro.getAngle();
+		
 		double throttle = joystick.getThrottle();
 		
-		double rotate = joystick.getX();
-		
-		double angle = gyro.getAngle();
-		
-		double accelX = accel.getX();
-		double accelY = accel.getY();
-		double accelZ = accel.getZ();
-		
-		double motorCurrent0 = pdp.getCurrent(0),
-			   motorCurrent1 = pdp.getCurrent(1),
-			   motorCurrent2 = pdp.getCurrent(14),
-			   motorCurrent3 = pdp.getCurrent(15);
+		double motor0Current    = pdp.getCurrent(0),
+			   motor1Current    = pdp.getCurrent(1),
+			   motor2Current    = pdp.getCurrent(14),
+			   motor3Current    = pdp.getCurrent(15),
+			   liftMotorCurrent = pdp.getCurrent(6);
 		
 		double compressorCurrent = compressor.getCompressorCurrent();
+		
+		final double safeLiftCurrent = 35;
+		
+		boolean gameDrive         = false,
+				liftMotorShutoff  = false,
+				liftMotorOverride = false,
+				stopLift          = false;
 		
 		/******** SMARTDASHBOARD ********/
 		
 		/** MOTORS **/
 		
-		SmartDashboard.putNumber("Motor 0 Current", motorCurrent0);
-		SmartDashboard.putNumber("Motor 1 Current", motorCurrent1);
-		SmartDashboard.putNumber("Motor 2 Current", motorCurrent2);
-		SmartDashboard.putNumber("Motor 3 Current", motorCurrent3);
+		SmartDashboard.putNumber("Motor 0 Current", motor0Current);
+		SmartDashboard.putNumber("Motor 1 Current", motor1Current);
+		SmartDashboard.putNumber("Motor 2 Current", motor2Current);
+		SmartDashboard.putNumber("Motor 3 Current", motor3Current);
+		
+		SmartDashboard.putNumber("Lift Motor Current", liftMotorCurrent);
 		
 		/** PNEUMATICS **/
 		
@@ -115,14 +86,11 @@ public class Robot extends IterativeRobot {
 		/** GYROSCOPE **/
 		
 		SmartDashboard.putNumber("Gyroscope Angle", angle);
-		SmartDashboard.putNumber("Accel X", accelX);
-		SmartDashboard.putNumber("Accel Y", accelY);
-		SmartDashboard.putNumber("Accel Z", accelZ);
 		
-		/******** CONTROLLER ********/
+		/******** CONTROLS ********/
 		
 		/** PNEUMATICS **/
-		
+		SmartDashboard.putBoolean("Gearage Open", trigger );
 		if (trigger) {
 			solenoid.set(DoubleSolenoid.Value.kForward);
 		}
@@ -132,16 +100,35 @@ public class Robot extends IterativeRobot {
 		
 		/** LIFT **/
 		
-		liftMotor.set(throttle);
+		if(stopButton) {
+			stopLift = !stopLift;
+		}
+		
+		if(overrideButton) {
+			liftMotorOverride = true;
+		}
+		
+		if((!liftMotorShutoff || liftMotorOverride) && !stopLift) {
+			liftMotor.set(throttle);
+		}
+		
+		if(liftMotorCurrent > safeLiftCurrent) {
+			liftMotorShutoff = true;
+		}
 		
 		/** DRIVE **/
 		
-		if (joystick.getY() <= 0)
-			rotate = -rotate;
+		if(modeButton) {
+			gameDrive = gameDrive ? false : true;
+		}
+		
+		if(gameDrive) {
+			if (joystick.getY() <= 0) {
+				rotate = -rotate;
+			}
+		}
 		
 		driveTrain.arcadeDrive(joystick.getY(), rotate);
-		
-		loop++;
 	}
 
 	@Override
